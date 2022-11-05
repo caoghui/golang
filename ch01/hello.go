7d8b24fd94ca01ccabbcf5340fd3257ffe53a3b6
bruce <bruce.cao@aushytech.com>
2022-11-05 14:24:24 +0800

    更新代码

--- ch01/hello.go
+++ ch01/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
 
 func main() {
 	fmt.Println("Hello World...")
+	log.Println("message")
+	//log.Fatalln("fatal message")
+	log.Panicln("panic message")
 }
